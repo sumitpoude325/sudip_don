3d34d6cb45e497818b70325ff59c56508d0a162d
sumitpoudel <sumitpoudel325@gmail.com>
2024-08-07 20:15:27 +0545

    more smooth and vanished and opasity used

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.querySelector("button").addEventListener("click", function () {
   document.querySelector(".ok").classList.toggle("show");
+  document.querySelector(".bottom").classList.toggle("vanish");
+  for (i = 0; i < 2; i++) {
+    document.querySelectorAll("hr")[i].classList.toggle("vanish");
+  }
 });
